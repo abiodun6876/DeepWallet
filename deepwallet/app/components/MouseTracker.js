5601e63687fefe04912d6b7e117a8254221c9864
AKINPELU Abiodun Moses <abiodunmosesakinpelu@gmail.com>
2023-10-13 02:23:15 -0700

    wallet,mouse,showtime
    update made by moses

--- deepwallet/app/components/MouseTracker.js
+++ deepwallet/app/components/MouseTracker.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MouseTracker = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -7,17 +7,32 @@ const MouseTracker = () => {
     setPosition({ x: e.clientX, y: e.clientY });
   };
 
+  useEffect(() => {
+    // Check if the code is running on the client side
+    if (typeof window !== 'undefined') {
+      // Add event listener for mousemove
+      window.addEventListener('mousemove', handleMouseMove);
+
+      // Clean up the event listener when the component unmounts
+      return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+      };
+    }
+  }, []);
+
   const calculateBackgroundColor = () => {
-    // Calculate background color based on mouse position
-    const red = Math.floor((position.x / window.innerWidth) * 255);
-    const green = Math.floor((position.y / window.innerHeight) * 255);
-    return `rgb(${red}, ${green}, 100)`;
+    if (typeof window !== 'undefined') {
+      // Calculate background color based on mouse position
+      const red = Math.floor((position.x / window.innerWidth) * 255);
+      const green = Math.floor((position.y / window.innerHeight) * 255);
+      return `rgb(${red}, ${green}, 100)`;
+    }
+    return ''; // Provide a default value if window is not defined
   };
 
   return (
     <div
       className="container mx-auto text-center mt-5"
-      onMouseMove={handleMouseMove}
       style={{ backgroundColor: calculateBackgroundColor() }}
     >
       <h1 className="text-2xl text-gray-800">Mouse Tracker</h1>
--- deepwallet/app/components/Wallet.js
+++ deepwallet/app/components/Wallet.js
@@ -1,4 +1,4 @@
-"use client"
+
 
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
